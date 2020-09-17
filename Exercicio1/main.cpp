3b5e55a2baea4fde864a9450518f197e77a8a311
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-17 19:59:59 -0300

    soma

--- Exercicio1/main.cpp
+++ Exercicio1/main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     int num1,num2,soma;
-    for (i=0; <=i; i++;)
+    int cont=0;
+
     {
         cout << "Entre com um numero" << endl;
-        cin>>num1,num2;
+        cin>>num1;
+        cout << "Entre com um numero" << endl;
+        cin>>num2;
 
         soma=num1+num2;
+
     }
-    cout<<"A soma é: "<<soma<<endl;
+    cout<<"A soma e: "<<soma<<endl;
     return 0;
 }
