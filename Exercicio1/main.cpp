3862037d551983f362723605e783720f0b5438c8
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-17 18:46:17 -0300

    Exercicio1
    Soma de dois numeros

---
+++ Exercicio1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+int num1,num2,soma;
+{
+    cout << "Entre com um numero" << endl;
+    cin>>num,num2;
+
+    soma=num1+num2;
+
+    cout<<"A soma é:"soma<<endl;
+    return 0;
+}
