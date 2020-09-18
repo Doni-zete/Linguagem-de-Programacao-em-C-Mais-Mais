b6695e658f7887f7d65630797add002c7fc73533
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-18 18:48:26 -0300

    Area de um triangulo

--- Exercicio03/main.cpp
+++ Exercicio03/main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main()
 {
     int altura,base,area;
-    cout<<"\t\t\tCalcular a area do triangulo\n\n"<<endl;
+    cout<<"\t\tCalcular a area do triangulo em Metros\n\n"<<endl;
     cout <<"\tEntre com a Base do Triangulo" << endl;
     cin>>base;
     cout<<"\tEntre com a altura"<<endl;
     cin>>altura;
     area =base*altura;
 
-    cout<<"A area do retangulo e:\n\n "<<area<<"m";
+    cout<<"A area do retangulo e:\n\n "<<area<<"m"<<endl;
     return 0;
 }
