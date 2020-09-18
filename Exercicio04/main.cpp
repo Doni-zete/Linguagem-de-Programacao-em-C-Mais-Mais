b0106df668826d2efaabdd8fa08fc25f7231a4bf
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-18 18:49:49 -0300

    Calcular hipotenusa do triangulo

---
+++ Exercicio04/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lado1,lado2,total;
+
+    cout << "Entre com um numero" << endl;
+    cin>>lado1;
+    cout<<"Entre com o segundo lado"<<endl;
+    cin>>lado2;
+
+    total=
+
+    return 0;
+}
