2b7d9a1451d5381628ed1858b038f806fb538a57
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-18 19:00:18 -0300

    Calculo da hipotenusa de um triângulo

--- Exercicio04/main.cpp
+++ Exercicio04/main.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 int main()
 {
-    int lado1,lado2,total;
+    int cateto1,cateto2,hipotenusa;
+    cout<<"\t\tCalcular a Hipotenusa do triangulo\n\n"<<endl;
+    cout << "Entre com um cateto1" << endl;
+    cin>>cateto1;
+    cout<<"Entre com o cateto2"<<endl;
+    cin>>cateto2;
 
-    cout << "Entre com um numero" << endl;
-    cin>>lado1;
-    cout<<"Entre com o segundo lado"<<endl;
-    cin>>lado2;
+    hipotenusa=(cateto1*cateto1)+(cateto2*cateto2);
 
-    total=
+    cout<<"A hipotenusa e: "<<hipotenusa;
 
     return 0;
 }
