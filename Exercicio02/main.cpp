ed6a208d9f89541bd7566f573b4e4c89b1cb58f9
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-18 09:09:53 -0300

    Dobro de um numero

--- Exercicio02/main.cpp
+++ Exercicio02/main.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main()
-int num, dobro;
-
-
 {
-    cout << "Entre com um numero" << endl;
-    cin>>num1;
+    int num1, dobro;
 
+    {
+        cout << "Entre com um numero" << endl;
+        cin>>num1;
 
+        dobro=num1*2;
+
+        cout<<"O dobro desse numero e: "<<dobro<<endl;
+    }
     return 0;
 }
