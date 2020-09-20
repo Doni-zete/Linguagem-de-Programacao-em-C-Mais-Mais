f6beb200135558ca9d3812b675efb0a892c70952
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-20 19:16:06 -0300

    imprimir a temperatura vetor

--- Aulavetor/main.cpp
+++ Aulavetor/main.cpp
@@ -5,23 +5,51 @@ using namespace std;
 
 int main()
 {
-    int vet[MAX], i;
+    int vet[MAX], i,soma;
     int vet2[MAX];
+    float media;
+
     for(i=0; i<MAX; i++)
     {
-        cout << "\nEntre com um numero" << endl;
-    cin>>vet[i];
+        cout << "\nEntre com uma temperatuta" << endl;
+        cin>>vet[i];
     }
 
 
-for(i=0;i<MAX;i++)
-    vet2[i]=vet[i];
+    for(i=0; i<MAX; i++)
+        vet2[i]=vet[i];
 
-cout<<endl<<endl;
+    cout<<endl<<endl;
 
-for(i=0; i<MAX; i++)
-    cout<<endl<<vet[i]<<"\t\t"<<vet2[i];
+    cout<<"vet1\t\tvet2";
+    for(i=0; i<MAX; i++)
+        cout<<endl<<vet[i]<<"\t\t"<<vet2[i];
 
 
-return 0;
+    for(i=0; i<MAX; i++)
+        vet[i]=vet2[(MAX-1)-i];
+
+    for(i=0; i<MAX; i++)
+    {
+        soma=soma+vet[i];
+    }
+
+
+    for(i=0; i<MAX; i++)
+    {
+        if(vet[i]>media)
+        {
+            media++;
+            media=soma/(float)MAX;
+        }
+    }
+
+    cout<<endl<<endl;
+
+    cout<<"vet\t\tvet2";
+    for(i=0; i<MAX; i++)
+
+        cout<<endl<<vet[i]<<"\t\t"<<vet2[i]<<endl;
+    cout<<"A media e:"<<media;
+    return 0;
 }
