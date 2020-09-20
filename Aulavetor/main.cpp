b487d85b74fcb69200a481b4677ef5da57dacf1b
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-20 09:54:53 -0300

    Aulavetor

---
+++ Aulavetor/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define max 10
+
+using namespace std;
+
+int main()
+{
+    int vet[max],i;
+    cout << "Entre com um numero" << endl;
+    cin>>vet[i];
+
+
+    return 0;
+}
