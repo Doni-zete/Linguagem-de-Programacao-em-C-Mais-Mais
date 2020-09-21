7319cf72f7b50ff4437834ca8b92bf15d39a5083
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-20 21:36:46 -0300

    Vetor temperatura
    Insere temperaturas, Mostra temperaturas Calcula média, Calcula maior temperatura, Calcula menor temperatura. Sair

--- Aulavetor2/main.cpp
+++ Aulavetor2/main.cpp
@@ -6,72 +6,123 @@ using namespace std;
 
 int main()
 {
-    const int insere=1,mostra=2,calculaMedia=3,maiorTemperatura=4,menorTemperatura=5,Sair=6;
+    const int insere=1,mostra=2,calculaMedia=3,maiorTemperatura=4,menorTemperatura=5,sair=6;
     int vet[MAX], i,soma;
     int vet2[MAX];
     float media;
     int escolhe;
+    int saida = 1;
+    int maior;
+    int menor;
 
-    cout<<"\t\t\tMenu\n";
-
-    cout<<"\n\t\t1-Insere temperaturas";
-    cout<<"\n\t\t2-Mostra temperaturas";
-    cout<<"\n\t\t3-Calcula média";
-    cout<<"\n\t\t4-Calcula maior temperatura";
-    cout<<"\n\t\t5-Calcula menor temperatura";
-    cout<<"\n\t\t6-Sair\n\n";
-
-    cout <<"\t\tEscolha um numero:" << endl;
-    cin>>escolhe;
-    while(escolhe==1)
+    while(saida!=2)
     {
+        cout<<"\t\t\tMenu\n";
+
+        cout<<"\n\t\t1-Insere temperaturas";
+        cout<<"\n\t\t2-Mostra temperaturas";
+        cout<<"\n\t\t3-Calcula média";
+        cout<<"\n\t\t4-Calcula maior temperatura";
+        cout<<"\n\t\t5-Calcula menor temperatura";
+        cout<<"\n\t\t6-Sair\n\n";
 
 
-
-    for(i=0; i<MAX; i++)
-    {
-        cout << "\nEntre com uma temperatuta" << endl;
-        cin>>vet[i];
-    }
-
-    for(i=0; i<MAX; i++)
-        vet2[i]=vet[i];
-
-    cout<<endl<<endl;
-
-    cout<<"vet1\t\tvet2";
-    for(i=0; i<MAX; i++)
-        cout<<endl<<vet[i]<<"\t\t"<<vet2[i];
+        cout <<"\t\tEscolha um numero:" << endl;
+        cin>>escolhe;
 
 
-    for(i=0; i<MAX; i++)
-        vet[i]=vet2[(MAX-1)-i];
-
-    for(i=0; i<MAX; i++)
-    {
-        soma=soma+vet[i];
-    }
-
-
-    for(i=0; i<MAX; i++)
-    {
-        if(vet[i]>media)
+        switch (escolhe)
         {
-            media++;
-            media=soma/(float)MAX;
+        case insere:
+        {
+            for(i=0; i<MAX; i++)
+            {
+                cout << "\nEntre com uma temperatuta" << endl;
+                cin>>vet[i];
+            }
+
+            for(i=0; i<MAX; i++)
+                vet2[i]=vet[i];
+
+            cout<<endl<<endl;
+
+            for(i=0; i<MAX; i++)
+                vet[i]=vet2[(MAX-1)-i];
+
+            for(i=0; i<MAX; i++)
+            {
+                if(vet[MAX]>media)
+                {
+                    media++;
+
+                }
+            }
+
+            maior =vet[0];
+            break;
         }
+
+        case mostra:
+        {
+
+            cout<<"vet1\t\tvet2";
+            for(i=0; i<MAX; i++)
+                cout<<endl<<vet[i]<<"\t\t"<<vet2[i];
+            cout<<endl<<endl;
+
+            cout<<"vet\t\tvet2";
+            for(i=0; i<MAX; i++)
+                cout<<endl<<vet[i]<<"\t\t"<<vet2[i]<<endl;
+            break;
+        }
+
+        case calculaMedia:
+        {
+            for(i=0; i<MAX; i++)
+            {
+                soma=soma+vet[i];
+                media=soma/(float)MAX;
+            }
+             cout<<"A media e:"<<media;
+            break;
+        }
+
+        case maiorTemperatura:
+        {
+            for(i=0; i<MAX; i++)
+            {
+                if(vet[i]>maior)
+                {
+                    maior=vet[i];
+                }
+            }
+            cout<<"A maior temperatura e: "<<maior;
+            cout<<endl<<endl;
+            break;
+
+        }
+        case menorTemperatura:
+        {
+            for(i=0; i<MAX; i++)
+            {
+                if(vet[i]<menor)
+                {
+                    menor=vet[i];
+                }
+            }
+            cout<<"A MENOR temperatura e: "<<menor;
+            cout<<endl<<endl;
+            break;
+        }
+        case sair:
+        {
+            saida =2;
+            cout<<"Voce saiu";
+            break;
+
+        }
+        }
+
     }
-
-    cout<<endl<<endl;
-
-    cout<<"vet\t\tvet2";
-    for(i=0; i<MAX; i++)
-
-        cout<<endl<<vet[i]<<"\t\t"<<vet2[i]<<endl;
-    cout<<"A media e:"<<media;
-    return 0;
-
-    }
-
     return 0;
 }
