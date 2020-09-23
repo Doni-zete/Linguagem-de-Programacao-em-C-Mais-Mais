96b6fcb7032dbcfe0490c8720882e873927b0dda
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-23 17:36:50 -0300

    Somar e multiplcar o resultado pelo primeiro valor

---
+++ Exercicio05/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2, resultado,guarda;
+
+    cout<<"Entre com um numero";
+    cin>>num1;
+    cout<<"Entre com outro numero";
+    cin>>num2;
+
+    resultado=num1+num2;
+
+
+    cout << "O resultado da soma e: "<<resultado << endl;
+    return 0;
+}
