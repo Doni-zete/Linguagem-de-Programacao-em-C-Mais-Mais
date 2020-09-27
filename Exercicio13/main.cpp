7ce736a6ba26052d0d3be74348be4d6db65586b4
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-27 13:31:51 -0300

    Ecreva um programa que receba o peso e a altura de n pessoas

--- Exercicio13/main.cpp
+++ Exercicio13/main.cpp
@@ -4,20 +4,61 @@ using namespace std;
 
 int main()
 {
-    float peso, maiorPeso,menorPeso,altura,menorAltura;
-    float maiorAltura, mediaPesos,mediaAlturas,mediaImc,maiorImc,menorImc;
+    float peso,altura,imc;
+    int cont=0,adicionaPeso=0,adicionaAltura=0,adicionaImc=0;
+    float maiorPeso=-99999, maiorAltura=-99999,maiorImc=-99999;
+    float menorPeso=99999,menorAltura=99999,menorImc=99999;
     char cadastrar='s';
-    while (cadastrar=='s'){
 
-    cout<<"Entre com o seu peso"<<endl;
-    cin>>peso;
-    cout<<"Entre com sua altura"<<endl;
-    cin>>altura;
+    while (cadastrar=='s')
+    {
+
+        cout<<"Entre com o seu peso"<<endl;
+        cin>>peso;
+        cout<<endl;
+        cout<<"Entre com sua altura"<<endl;
+        cin>>altura;
+        cout<<endl<<endl;
+        adicionaPeso+=peso;
+        adicionaAltura+=altura;
+        cont++;
+        imc=peso/(altura*altura);
+        adicionaImc+=imc;
+
+        if(peso>maiorPeso)
+        {
+            maiorPeso=peso;
+        }
+        if(peso<menorPeso)
+        {
+            menorPeso=peso;
+        }
+        if(altura<menorAltura)
+        {
+            menorAltura=altura;
+        }
+        if(altura>maiorAltura)
+        {
+            maiorAltura=altura;
+        }
+        if(imc> maiorImc)
+        {
+            maiorImc=imc;
+        }
+        if(imc<menorImc)
+        {
+            menorImc=imc;
+        }
 
 
 
-    cout << "Deseja cadastrar mais? (s SIM)ou (n NAO) " << endl;
-    cin>>cadastrar;
+
+        cout <<"\tDeseja cadastrar mais? (s SIM)ou (n NAO) " << endl;
+        cin>>cadastrar;
+        cout<<endl;
     }
+    cout<<"\nO maior peso e: "<<maiorPeso<<endl;
+    cout<<"\nO menor peso e: "<<menorPeso<<endl;
+
     return 0;
 }
