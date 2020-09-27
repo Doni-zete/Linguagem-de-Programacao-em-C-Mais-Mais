ed12c8ebcdd6c1919ee36ce92f4acb67fe8fd30e
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-09-26 22:40:32 -0300

    Area do quadrado

---
+++ Exercicio08/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lado1,lado2,area;
+    cout << "Entre com o lado do quadrado" << endl;
+    cin>>lado1;
+    cout<<"Entre com outro lado"<<endl;
+    cin>>lado2;
+
+    area=lado1*lado2;
+
+    cout<<"A area do quadrado e "<<area<<endl;
+    return 0;
+}
