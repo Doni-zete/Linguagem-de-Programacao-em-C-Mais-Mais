af78843a23ba38eda1054c85147b2e9713a7f223
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-02 21:34:14 -0300

    Maior menor e somatoria dos numeros impares e pares

--- Exercicio14/main.cpp
+++ Exercicio14/main.cpp
@@ -1,11 +1,117 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
+
 int main()
+
 {
-    int ano;
-    float preco,diaFumado
-    cout << "Hello world!" << endl;
-    return 0;
+
+    int vet[10], qde_par, acc_impar,maior_par, menor_impar;
+
+    int i, cont;
+
+    qde_par=0;
+    acc_impar=0;
+
+    for (i=0; i<10; i++)
+
+    {
+
+        cout << "\nDigite um numero: ";
+
+        cin >> vet[i];
+
+    }
+
+
+
+    qde_par=0;
+
+    for (i=0; i<10; i++)
+
+        if (vet[i]%2==0)
+
+            qde_par++;
+
+    cout << "\n\nQuantidade de numeros pares: " << qde_par << "\n\n";
+
+
+
+    acc_impar=0;
+
+    for (i=0; i<10; i++)
+
+        if (vet[i]%2!=0)
+
+            acc_impar+=vet[i];
+
+    cout << "\n\nA somatoria de numeros impares: " << acc_impar << "\n\n";
+
+    cont=0;
+
+    for(i=0; i<10; i++)
+
+    {
+
+        if (vet[i]%2==0)
+
+        {
+
+            cont++;
+
+            if (cont==1)
+
+                maior_par=vet[i];
+
+            else
+
+                if (vet[i]>maior_par)
+
+                    maior_par=vet[i];
+
+        }
+
+    }
+
+
+
+    cout << "\n\nMaior par: " << maior_par << "\n\n";
+
+
+
+
+
+    cont=0;
+
+    for(i=0; i<10; i++)
+
+    {
+
+        if (vet[i]%2!=0)
+
+        {
+
+            cont++;
+
+            if (cont==1)
+
+                menor_impar=vet[i];
+
+            else
+
+                if (vet[i]<menor_impar)
+
+                    menor_impar=vet[i];
+
+        }
+
+    }
+
+
+
+    cout << "\n\nMenor impar: " << menor_impar << "\n\n";
+
+
+
 }
