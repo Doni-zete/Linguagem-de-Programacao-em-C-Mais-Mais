0d26f788af9e74804dec18a6ab6a30aa6c151440
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-08 16:33:15 -0300

    a

--- Exercicio18/main.cpp
+++ Exercicio18/main.cpp
@@ -10,9 +10,19 @@ int cubo(int x)
     return x*x*x;
 }
 
+void troca (int x, int y)
+{
+    int aux;
+    aux=x;
+    x=y;
+    y=aux;
+     cout << "\nx=" <<x <<"\ny="<<y<<"\n\n";
+}
+
 int main()
 {
-    int a=4;
-    cout << "\nO cubo de 4 e:" <<cubo(a) <<endl;
+    int a=4,b=5;
+    troca (a,b);
+    cout << "\na=" <<a <<"\nb="<<b<<"\n\n";
     return 0;
 }
