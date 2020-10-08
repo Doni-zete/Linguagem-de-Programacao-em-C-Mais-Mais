4518afabbb84475fc73b18a359d1578af589f2df
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-08 19:59:16 -0300

    Troca valores

--- Exercicio18/main.cpp
+++ Exercicio18/main.cpp
@@ -3,26 +3,25 @@
 
 using namespace std;
 
-void tela(void);
-int cubo(int x)
-{
 
-    return x*x*x;
-}
 
-void troca (int x, int y)
+void troca (int *x, int *y)
 {
     int aux;
-    aux=x;
-    x=y;
-    y=aux;
-     cout << "\nx=" <<x <<"\ny="<<y<<"\n\n";
+    aux=*x;
+    *x=*y;
+    *y=aux;
+    cout << "\nx=" <<*x <<"\ny="<<*y<<"\n\n";
 }
 
 int main()
 {
     int a=4,b=5;
-    troca (a,b);
+    cout << "\na=" <<a <<"\nb="<<b<<"\n\n";
+    troca (&a,&b);
     cout << "\na=" <<a <<"\nb="<<b<<"\n\n";
     return 0;
 }
+
+
+
