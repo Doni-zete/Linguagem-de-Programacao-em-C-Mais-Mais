d5c352dba006c063f977506ccc274f8474d42b33
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-17 21:16:46 -0300

    Pilha

--- Exercicio20/main.cpp
+++ Exercicio20/main.cpp
@@ -6,11 +6,17 @@ int main()
 {
 
     int pilha[MAX],topo,elemento,op,flag;
-    topo=11;
+    topo=-1;
     do{
         system("cls");
-    }
+
+
     cout << "******MENU******\n\n\n" << endl;
-    cout<<"\n1 - push"
+    cout<<"\n1 - Push";
+    cout<<"\n2 - Pop";
+    cout<<"\n3 - Imprime pilha";
+    cout<<"\n9 - Sair";
+
+    }while(op!=9);
     return 0;
 }
