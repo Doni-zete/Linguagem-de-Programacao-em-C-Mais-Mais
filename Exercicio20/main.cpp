42bdf006cad862d5b1c7d15286d41ba09fea118e
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-17 21:38:51 -0300

    pilha

--- Exercicio20/main.cpp
+++ Exercicio20/main.cpp
@@ -8,18 +8,34 @@ int main()
 
     int pilha[MAX],topo,elemento,op,flag;
     topo=-1;
-    do{
+    do
+    {
         system("cls");
 
 
-    cout << "******MENU******\n\n\n" << endl;
-    cout<<"\n1 - Push";
-    cout<<"\n2 - Pop";
-    cout<<"\n3 - Imprime pilha";
-    cout<<"\n9 - Sair";
-    cout<<"\n\n Digite sua opcao -> ";
-    cin>>op;
+        cout << "******MENU******\n\n\n" << endl;
+        cout<<"\n1 - Push";
+        cout<<"\n2 - Pop";
+        cout<<"\n3 - Imprime pilha";
+        cout<<"\n9 - Sair";
+        cout<<"\n\n Digite sua opcao -> ";
+        cin>>op;
 
-    }while(op!=9);
-    return 0;
-}
+        switch(op)
+        {
+        case 1 : system("cls");
+
+
+                  cout<<"PUSH ";
+                  cout<<"Digite o elemento a ser empilhado";
+                  cin>>elemento;
+                  flag =  push(pilha,&topo,elemento);
+                  if(flag==0)
+                  cout<<"\n\nElemento inseriod com sucesso!";
+                  elsecout<<"\nn\Impossivel inserir..\nPilha cheia";
+                  sytem("pause");
+
+        }
+    while(op!=9);
+        return 0;
+    }
