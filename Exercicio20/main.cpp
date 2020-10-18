c039363a85fbfcc82acc9fd32feccff6889e4f89
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-17 21:30:54 -0300

    pilha

--- Exercicio20/main.cpp
+++ Exercicio20/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define MAX 5
 
 using namespace std;
 
@@ -16,6 +17,8 @@ int main()
     cout<<"\n2 - Pop";
     cout<<"\n3 - Imprime pilha";
     cout<<"\n9 - Sair";
+    cout<<"\n\n Digite sua opcao -> ";
+    cin>>op;
 
     }while(op!=9);
     return 0;
