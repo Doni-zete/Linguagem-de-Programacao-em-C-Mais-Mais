679acbde2c87c2d31f6dcf6e0bc742f09df39116
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-25 21:40:33 -0300

    Update main.cpp

--- Exercicio21/main.cpp
+++ Exercicio21/main.cpp
@@ -6,25 +6,44 @@ using namespace std;
 
 int main()
 {
-    int fila[MAX], fim, op,elemento, flag;
+    int fila[MAX], fim, op,elemento, flag,insere_fila;
     fim=-1;
-    do{
-            system("cls");
-            cout << "**** MENU****\n\n\n" << endl;
-            cout<<"\n1 - Insere na fila";
-            cout<<"\n2 - Remove da fila";
-            cout<<"\n3 - Imprime fila";
-            cout<<"\n9 - Sair\n";
-            cout<<"\n    DIGITE A SUA OPCAO->";
-            cin>>op;
+    do
+    {
+        system("cls");
+        cout << "**** MENU****\n\n\n" << endl;
+        cout<<"\n1 - Insere na fila";
+        cout<<"\n2 - Remove da fila";
+        cout<<"\n3 - Imprime fila";
+        cout<<"\n9 - Sair\n";
+        cout<<"\n    DIGITE A SUA OPCAO->";
+        cin>>op;
 
-            switch(op)
+        switch(op)
 
-            {
-              case 1 : system("cls") ;
-              cout<<"****Insere na fila"
-            }
-    }while(op!=9)
+        {
+        case 1 :
+            system("cls") ;
+            cout<<"****Insere na fila****\n\n\n";
+            cout<<"Digite um numero inteiro para ser inserido na fila:";
+            cin>>elemento;
+            flag = insere_fila(fila,&fim,elemento);
+            if(flag==0)
 
-    return 0;
+                cout<<"\n\nElemento inserido com sucesso!!!\n\n";
+
+            else
+                cout<<"\n\nImpossivel inserir...\nFila cheia!\n\n";
+            system("pause");
+            break;
+
+
+
+
+
+        }
+    }
+    while(op!=9)
+
+        return 0;
 }
