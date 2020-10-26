51a9f58b388ac1fdc9aa87ca736aef260b8e7020
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-25 21:23:02 -0300

    Fila fim função
    2

--- Exercicio21/main.cpp
+++ Exercicio21/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <stdlib.h>
+#define MAX 10
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int fila[MAX], fim, op,elemento, flag;
+    fim=-1;
+    do{
+            system("cls");
+            cout << "**** MENU****\n\n\n" << endl;
+            cout<<"\n1 - Insere na fila";
+            cout<<"\n2 - Remove da fila";
+            cout<<"\n3 - Imprime fila";
+            cout<<"\n9 - Sair\n"
+;    }while(op!=9)
+
     return 0;
 }
