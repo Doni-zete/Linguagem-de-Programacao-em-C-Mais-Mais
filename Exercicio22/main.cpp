631ab775004773de64db824c557dbc343ec2f2ab
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-10-30 21:32:07 -0300

    aula22

--- Exercicio22/main.cpp
+++ Exercicio22/main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+
+int insere_lista(int l[], int*f, int e, int p);
+int remove_lista(int l[],int *f, int p );
+int imprime_lista(int l [], int f);
+
 int main()
 {
+
+    int lista[MAX],fim, elemento,posicao,flag,op;
     cout << "Hello world!" << endl;
     return 0;
 }
