94bc7a44452cf537acc4de4239d25902274454e4
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-11-24 23:30:16 -0300

    ajuste

--- Trabalho parte 2/Leitura de um texto caracter por caracter/main.cpp
+++ Trabalho parte 2/Leitura de um texto caracter por caracter/main.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include<stdio.h>
-#include<stdlib.h>
+//
+//	caesarCipher.cpp
+//	Created by David on 4/13/12
+//	Copyright 2013 TTU. All rights reserved.
 
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-
-
 int main()
 {
-  FILE*arquivo;
-  int i;
-  arquivo =fopen("texto.txt","r");
-  if(arquivo==NULL)
-  {
+	string input;
+	int count = 0, length;
 
-    printf("\nProblema ao abrir o arquivo\n\n");
-    return 0;
+	cout << "Enter your phrase: \n";
+	getline(cin, input);
 
-  }
+	length = (int)input.length();
 
+	for (count = 0; count < length; count++)
+	{
+		if (isalpha(input[count]))
+		{
+			input[count] = tolower(input[count]);
+			for (int i = 0; i < 13; i++)
+			{
+				if (input[count] == 'z')
+					input[count] = 'a';
+				else
+					input[count]++;
+			}
+		}
+	}
 
-
-
-  char texto[1000];
-  for (i=0;!feof(arquivo);i++)
-  {
-
-
-    texto[i]= fgetc(arquivo);
-    printf("%c",texto [i]);
-    for (int j=0;j<=100000000;j++);
-  }
-  return 0 ;
-
+	cout << "Results: \n"<<input<<endl;
 
 }
