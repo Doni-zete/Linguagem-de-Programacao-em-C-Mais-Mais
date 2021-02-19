4bc2f46099b441259aeb16017dbc43bfaf764b5f
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-18 22:36:27 -0300

    agenda do prof

---
+++ Trabalho parte 2/AgendaFuncionando/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "agenda.h"
+
+using namespace std;
+
+int main()
+//Tipo Pessoa
+// O que será armazenada na genda
+
+
+typedef struct{
+    string nome;
+    string email;
+    int dia;
+    int mes;
+    int ano;
+}Pessoa;
+//Tipo Agenda
+//Estrutura que armazena as pessoas
+typedef struct{
+  Pessoa
+
+}
+}
