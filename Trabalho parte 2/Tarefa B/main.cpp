1ad6a6d154fe322d220007247e5f4273fe044ae0
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-04-15 22:00:48 -0300

    tarefa b

--- Trabalho parte 2/Tarefa B/main.cpp
+++ Trabalho parte 2/Tarefa B/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+  int num1, num2;
+  int soma, subtrair,dividir, multiplicar,resultado;
+
+    cout << "\n\t***CALULADORA***" << endl;
+    cout<<"\nEntre com  um numero:"<<endl;
+    cin>>num1;
+
+    cout<<"\nEntre com o segundo numero:"<<endl;
+    cin>>num2;
+
+    cout<<"\nEscolha a operacao matematica para ser calculada:"<<endl;
+    cout<<"\n\t1-Somar";
+    cout<<"\n\t2-Subtrair";
+    cout<<"\n\t3-Multiplicar";
+    cout<<"\n\t4-Dividir"<<endl;
+
+
+
+
+
+
     return 0;
 }
